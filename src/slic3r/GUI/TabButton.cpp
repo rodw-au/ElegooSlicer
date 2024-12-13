12a39afb82a19d20a05adf729b1ed72619b24342
DESKTOP-DT9IR7H\anjis <anjis.zhou@elegoo.com>
2024-12-13 09:16:51 +0800

    Modify project page color

--- src/slic3r/GUI/TabButton.cpp
+++ src/slic3r/GUI/TabButton.cpp
@@ -17,7 +17,7 @@ END_EVENT_TABLE()
 static wxColour BORDER_HOVER_COL = wxColour(0, 120, 189);
 
 const static wxColour TAB_BUTTON_BG    = wxColour("#FEFFFF");
-const static wxColour TAB_BUTTON_SEL   = wxColour(219, 253, 213, 255);
+const static wxColour TAB_BUTTON_SEL   = wxColour(0, 120, 189, 255);
 
 TabButton::TabButton()
     : paddingSize(43, 16)

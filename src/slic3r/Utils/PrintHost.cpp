d15a0b9100a18ed0ef779b8e73fd5127cb36f3b0
anjis <anjis.zhou@elegoo.com>
2025-01-17 12:28:10 +0800

    feat: ElegooLink uploads and prints support time-lapse photography and other parameter configurations

--- src/slic3r/Utils/PrintHost.cpp
+++ src/slic3r/Utils/PrintHost.cpp
@@ -81,7 +81,7 @@ wxString PrintHost::format_error(const std::string &body, const std::string &err
         return wxString::Format("HTTP %u: %s", status, wxbody);
     } else {
         if (error.find("curl:Timeout was reached") != std::string::npos) {
-            return _L("Connection timed out. Please check if the printer and computer network are functioning properly.");
+            return _L("Connection timed out. Please check if the printer and computer network are functioning properly, and confirm that they are on the same network.");
         }else if(error.find("curl:Couldn't resolve host name")!= std::string::npos){
             return _L("The Hostname/IP/URL could not be parsed, please check it and try again.");
         } else if (error.find("Connection was reset") != std::string::npos){
